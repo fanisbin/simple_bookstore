557bb1b07aacc18da71a40a831a9c4515953e906
fanisbin <101002559+fanisbin@users.noreply.github.com>
2022-03-10 17:20:43 +0200

    3

--- vivliopoleio/src/vivliopoleio/Store.java
+++ vivliopoleio/src/vivliopoleio/Store.java
@@ -5,6 +5,7 @@ public class Store {
 	private String link;
 	private String phone;
 	private String email;
+	protected Product p;
 	
 	// vasikos constructor ths klashs store
 	public Store(String link, String phone, String email) {
@@ -12,12 +13,23 @@ public class Store {
 		this.link = link;
 		this.phone = phone;
 		this.email = email;
+		this.p = new Product();
 		
 	}
 	
+	// emfanisei vasikwn stoixeiwn katasthmatos
 	public String stoixeia() {
 		
 		return "\nLink: " + link + "\nStoixeia Epikoinwnias \nthlefwno: " + phone + "\nemail: " + email;
 	}
+	
+	//prosthiki proiontos
+	public void add_prod(String category, String available, double price, String desc) {
+		
+		this.p = new Product(category, available, price, desc);
+	}
+	
+	
+	
 
 }
