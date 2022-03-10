cc9ac945492df8b16e779aeeed5f1498af35b028
fanisbin <101002559+fanisbin@users.noreply.github.com>
2022-03-10 16:54:51 +0200

    first commit with basic classes

---
+++ vivliopoleio/src/vivliopoleio/Main.java
@@ -0,0 +1,12 @@
+package vivliopoleio;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+			
+		
+		
+	}
+
+}
---
+++ vivliopoleio/src/vivliopoleio/product.java
@@ -0,0 +1,61 @@
+package vivliopoleio;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class product {
+	
+	private String category;
+	private String available;
+	private double price;
+	private String desc;
+	private int id;
+	
+	// static metavliti gia thn automath aukshsh tou id kata ena kathe fora pou prosthetw ena product
+	private static final AtomicInteger autoid = new AtomicInteger(0); 
+	
+
+
+	//constructor me arxikopoihsh timwn me mhdenikes/kenes times
+	public product() {
+		this.category = " ";
+		this.available = " ";
+		this.price = 0;
+		this.desc = " ";
+		this.id = autoid.incrementAndGet();
+	}
+	
+	// constructor me parametrous
+	public product(String category, String available, double price, String desc) {
+		
+		this.category = category;
+		this.available = available;
+		this.price = price;
+		this.desc = desc;
+		this.id = autoid.incrementAndGet();
+	}
+	
+	// xarakthrismos proiodos ws diathesimo
+	public void avail_yes() {
+		this.available = "yes";
+	}
+	
+	// xaraktirismos proiodos ws mh diathesimo
+	public void avail_no() {
+		this.available = "no";
+	}
+	
+	// elegxos an ena proion einai diathesimo
+	public int get_availability() {
+		if (this.available.equals("yes")) return 1;
+		else return 0;
+	}
+	
+	//emfanhsh stoixeiwn enos proiodos
+	public String toString() {
+		
+		return "\nkathgoria: " + this.category + "\ndiathesimo: " + this.available + "\ntimh: " + this.price + "\nperigrafh: " + this.desc + 
+				"\nkwdikos proiodos: " + id;
+		
+	}
+	
+}
